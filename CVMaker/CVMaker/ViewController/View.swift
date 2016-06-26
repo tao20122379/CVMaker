090cc4656044fa1084abdf9cddda99515292b090
tao20122379 <anhtaotsbn@gmail.com>
2016-06-26 23:57:38 +0700

    Create Interface

---
+++ CVMaker/CVMaker/ViewController/View.swift
@@ -0,0 +1,13 @@
+//
+//  View.swift
+//  CVMaker
+//
+//  Created by khactao on 6/26/16.
+//  Copyright © 2016 khactao. All rights reserved.
+//
+
+import UIKit
+
+class View: UINavigationController{
+    
+}
--- CVMaker/DerivedData/CVMaker/Build/Intermediates/CVMaker.build/Debug-iphonesimulator/CVMaker.build/DerivedSources/CVMaker-Swift.h
+++ CVMaker/DerivedData/CVMaker/Build/Intermediates/CVMaker.build/Debug-iphonesimulator/CVMaker.build/DerivedSources/CVMaker-Swift.h
@@ -98,6 +98,8 @@ typedef int swift_int4  __attribute__((__ext_vector_type__(4)));
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
 #pragma clang diagnostic ignored "-Wduplicate-method-arg"
 @class UIWindow;
+@class UINavigationController;
+@class HomeViewController;
 @class UIApplication;
 @class NSObject;
 @class NSURL;
@@ -108,6 +110,8 @@ typedef int swift_int4  __attribute__((__ext_vector_type__(4)));
 SWIFT_CLASS("_TtC7CVMaker11AppDelegate")
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (nonatomic, strong) UIWindow * _Nullable window;
+@property (nonatomic, strong) UINavigationController * _Nullable nav;
+@property (nonatomic, strong) HomeViewController * _Nullable homeVC;
 - (BOOL)application:(UIApplication * _Nonnull)application didFinishLaunchingWithOptions:(NSDictionary * _Nullable)launchOptions;
 - (void)applicationWillResignActive:(UIApplication * _Nonnull)application;
 - (void)applicationDidEnterBackground:(UIApplication * _Nonnull)application;
@@ -132,5 +136,42 @@ SWIFT_CLASS("_TtC7CVMaker14ViewController")
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
 @end
+
+
+SWIFT_CLASS("_TtC7CVMaker26CVFormSelectViewController")
+@interface CVFormSelectViewController : ViewController
+- (void)viewDidLoad;
+- (void)didReceiveMemoryWarning;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+SWIFT_CLASS("_TtC7CVMaker16CVViewController")
+@interface CVViewController : ViewController
+- (void)viewDidLoad;
+- (void)didReceiveMemoryWarning;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+SWIFT_CLASS("_TtC7CVMaker18HomeViewController")
+@interface HomeViewController : ViewController
+- (void)viewDidLoad;
+- (void)didReceiveMemoryWarning;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+SWIFT_CLASS("_TtC7CVMaker4View")
+@interface View : UINavigationController
+- (nonnull instancetype)initWithNavigationBarClass:(Class _Nullable)navigationBarClass toolbarClass:(Class _Nullable)toolbarClass OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithRootViewController:(UIViewController * _Nonnull)rootViewController OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+@end
+
 
 #pragma clang diagnostic pop
